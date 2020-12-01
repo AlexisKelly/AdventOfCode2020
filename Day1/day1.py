8c71fa699f46c99c83c14abf85286450888ff73a
AlexisKelly <Alexis.Kelly@bjss.co.uk>
2020-12-01 10:52:46 +0000

    Day 1 Part 2 Added

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,22 +1,30 @@
 with open("Day1/day1input.txt") as f:
     day1Input = f.readlines()
 found = False 
+printedPart1 = False
+printedPart2 = False
 
 for i in range(len(day1Input)):
     for j in range(len(day1Input)):
+        for k in range(len(day1Input)):
+            input1 = int(day1Input[i])
+            input2 = int(day1Input[j])
+            input3 = int(day1Input[k])
 
-        input1 = int(day1Input[i])
-        input2 = int(day1Input[j])
+            sumValue=0
+            sumValue = input1 +input2
+            
+            part2SumValue = sumValue + input3
 
-        sumValue=0
-        sumValue = input1 +input2
-       # print("Sum is "+str(input1)+"+"+str(input2)+"="+str(sumValue))
-        
-        if(sumValue==2020):
-            multipliedValues = input1*input2
-            print("Sum is "+day1Input[i]+"+"+day1Input[j]+"="+str(sumValue))
-            print("***********"+str(multipliedValues)+"*************")
-            found=True
-            break
-    if found == True:
-        break
+            if(sumValue==2020 and printedPart1 == False):
+                multipliedValues = input1*input2
+                print("Part 1 Sum is "+str(input1)+"+"+str(input2)+"="+str(sumValue))
+                print("***********PART 1 ***"+str(multipliedValues)+"*************")
+                printedPart1 = True
+
+
+            if(part2SumValue==2020 and printedPart2 == False):
+                part2MultipliedValues = input1*input2*input3
+                print("Part 2 Sum is "+str(input1)+"+"+str(input2)+"+"+str(input3)+"="+str(part2SumValue))
+                print("***********PART 2 ***"+str(part2MultipliedValues)+"*************")
+                printedPart2 = True
